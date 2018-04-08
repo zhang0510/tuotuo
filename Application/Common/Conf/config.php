b666aa9f16b45d3af90d08aa4a7f249bdf15583a
zwen <18514430012@163.com>
2018-04-08 18:01:05 +0800

    首页修改

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -59,7 +59,7 @@ return array(
 	),
 	/* 数据库设置 */
 	'DB_TYPE' =>  'mysql',     // 数据库类型
-	'DB_HOST' =>  '127.0.0.1', // 服务器地址
+	'DB_HOST' =>  '120.27.96.249', // 服务器地址
 	'DB_NAME' =>  'ttyc1',          // 数据库名
 	'DB_USER' =>  'root',      // 用户名
 	'DB_PWD'  =>  'tuotuo123sql&',          // 密码
