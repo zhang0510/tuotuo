e94f71be8393a8d95cd27f79961224a5d6c3d4c4
zwen <18514430012@163.com>
2018-04-20 09:26:03 +0800

    统计代码提交

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -60,7 +60,7 @@ return array(
 	/* 数据库设置 */
 	'DB_TYPE' =>  'mysql',     // 数据库类型
 	'DB_HOST' =>  '120.27.96.249', // 服务器地址
-	'DB_NAME' =>  'ttyc1',          // 数据库名
+	'DB_NAME' =>  'ttyc',          // 数据库名
 	'DB_USER' =>  'root',      // 用户名
 	'DB_PWD'  =>  'tuotuo123sql&',          // 密码
 	'DB_PORT' =>  '3306',        // 端口
